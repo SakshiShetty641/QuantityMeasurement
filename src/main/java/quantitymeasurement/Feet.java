c4dfe9f370181523aae51da873e32dd0154a855e
Sakshi <sakshishetty1231997@gmail.com>
2021-09-20 15:19:46 +0530

    Added: Code and test cases to check equality of two feets

---
+++ src/main/java/quantitymeasurement/Feet.java
@@ -0,0 +1,23 @@
+package quantitymeasurement;
+
+/**
+ * Purpose - To simulate a Quantity Management System
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-09-20
+ */
+public class Feet {
+    private final double value;
+
+    public Feet(double value){
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+}
---
+++ src/test/java/quantitymeasurement/QuantityTest.java
@@ -0,0 +1,14 @@
+package quantitymeasurement;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class QuantityTest {
+
+    @Test
+    public void given0Feetand0Feet_ShouldReturnEqual(){
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+}
