e01a0888330a223e05b5a3b4d05462494d95b5f1
Sakshi <sakshishetty1231997@gmail.com>
2021-09-22 23:51:59 +0530

    Added: Code and test cases to compare temperature entity

--- src/main/java/quantitymeasurement/Temperature.java
+++ src/main/java/quantitymeasurement/Temperature.java
@@ -1,7 +1,7 @@
 package quantitymeasurement;
 
 public class Temperature {
-    public enum Unit {FAHRENHEIT}
+    public enum Unit {FAHRENHEIT, CELSIUS}
 
     private final Unit unit;
     private final double temperature;
@@ -9,6 +9,12 @@ public class Temperature {
     public Temperature(Unit unit, double temperature) {
         this.unit = unit;
         this.temperature = temperature;
+    }
+
+    public boolean compare(Temperature that) {
+        if (this.unit.equals(Unit.FAHRENHEIT) && that.unit.equals(Unit.CELSIUS))
+            return Double.compare((this.temperature - 32) * 5 / 9, that.temperature) == 0;
+        return false;
     }
 
     @Override
--- src/test/java/quantitymeasurement/TemperatureTest.java
+++ src/test/java/quantitymeasurement/TemperatureTest.java
@@ -39,4 +39,12 @@ public class TemperatureTest {
         Temperature temperature2 = new Temperature(Temperature.Unit.FAHRENHEIT, 1.0);
         Assertions.assertNotEquals(temperature1, temperature2);
     }
+
+    @Test
+    public void given212FahrenheitAnd100Celsius_WhenCompared_ShouldReturnEqualTemperature() {
+        Temperature fahrenheit = new Temperature(Temperature.Unit.FAHRENHEIT, 212.0);
+        Temperature celsius = new Temperature(Temperature.Unit.CELSIUS, 100.0);
+        boolean compareCheck = fahrenheit.compare(celsius);
+        Assertions.assertTrue(compareCheck);
+    }
 }
