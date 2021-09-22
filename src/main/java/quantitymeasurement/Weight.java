58888e8be11391e6872be7112f48a487591e2b1f
Sakshi <sakshishetty1231997@gmail.com>
2021-09-22 23:10:38 +0530

    Added: Code and test cases to compare weight entities

--- src/main/java/quantitymeasurement/Weight.java
+++ src/main/java/quantitymeasurement/Weight.java
@@ -2,7 +2,10 @@ package quantitymeasurement;
 
 
 public class Weight {
-    enum Unit {GRAM};
+    enum Unit {KILOGRAM, GRAM, TONNE};
+
+    private static final double KILOGRAM_TO_GRAM = 1000;
+    private static final double TONNE_TO_KILOGRAM = 1000;
 
     private final Unit unit;
     private final double value;
@@ -11,6 +14,19 @@ public class Weight {
         this.unit = unit;
         this.value = value;
     }
+
+    public boolean compare(Weight that) {
+        if (this.unit.equals(Unit.KILOGRAM) && that.unit.equals(Unit.GRAM))
+            return Double.compare(this.value * KILOGRAM_TO_GRAM, that.value) == 0;
+        if (this.unit.equals(Unit.GRAM) && that.unit.equals(Unit.KILOGRAM))
+            return Double.compare(this.value , that.value* KILOGRAM_TO_GRAM) == 0;
+        if (this.unit.equals(Unit.TONNE) && that.unit.equals(Unit.KILOGRAM))
+            return Double.compare(this.value * TONNE_TO_KILOGRAM, that.value) == 0;
+        if (this.unit.equals(Unit.KILOGRAM) && that.unit.equals(Unit.TONNE))
+            return Double.compare(this.value, that.value* TONNE_TO_KILOGRAM) == 0;
+        return false;
+    }
+
 
     @Override
     public boolean equals(Object o) {
--- src/test/java/quantitymeasurement/WeightTest.java
+++ src/test/java/quantitymeasurement/WeightTest.java
@@ -23,7 +23,7 @@ public class WeightTest {
     public void given0GramandNULLGram_ShouldReturnNotEqual(){
         Weight gram1 = new Weight(Weight.Unit.GRAM,0.0);
         Weight gram2 = null;
-        Assertions.assertEquals(gram1, gram2);
+        Assertions.assertNotEquals(gram1, gram2);
     }
 
     @Test
@@ -38,5 +38,37 @@ public class WeightTest {
         Weight gram1 = new Weight(Weight.Unit.GRAM,0.0);
         Weight gram2 = new Weight(Weight.Unit.GRAM,1.0);
         Assertions.assertNotEquals(gram1, gram2);
+    }
+
+    @Test
+    public void given1KilogramAnd1000Gram_WhenCompared_ShouldReturnEqualWeight() {
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1.0);
+        Weight gram1 = new Weight(Weight.Unit.GRAM, 1000.0);
+        boolean compareCheck = kilogram1.compare(gram1);
+        Assertions.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1000GramAnd1Kilogram_WhenCompared_ShouldReturnEqualWeight(){
+        Weight gram1 = new Weight(Weight.Unit.GRAM, 1000.0);
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1.0);
+        boolean compareCheck = gram1.compare(kilogram1);
+        Assertions.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1TonneAnd1000Kilogram_WhenCompared_ShouldReturnEqualWeight() {
+        Weight tonne1 = new Weight(Weight.Unit.TONNE, 1.0);
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1000.0);
+        boolean compareCheck = tonne1.compare(kilogram1);
+        Assertions.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1000KilogramAnd1Tonne_WhenCompared_ShouldReturnEqualWeight() {
+        Weight kilogram1 = new Weight(Weight.Unit.KILOGRAM, 1000.0);
+        Weight tonne1 = new Weight(Weight.Unit.TONNE, 1.0);
+        boolean compareCheck = tonne1.compare(kilogram1);
+        Assertions.assertTrue(compareCheck);
     }
 }
