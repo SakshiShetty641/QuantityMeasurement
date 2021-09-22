c89919a5f7dd30cb1f5347fb8bc56aabc68692ff
Sakshi <sakshishetty1231997@gmail.com>
2021-09-22 23:44:57 +0530

    Added: Code and test cases to check equality of two temperatures

---
+++ src/test/java/quantitymeasurement/TemperatureTest.java
@@ -0,0 +1,42 @@
+package quantitymeasurement;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TemperatureTest {
+
+    @Test
+    void given0FahrenheitAnd0Fahrenheit_ShouldReturnEqualTemperature() {
+        Temperature temperature1 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Temperature temperature2 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Assertions.assertEquals(temperature1, temperature2);
+    }
+
+    @Test
+    void given0FahrenheitAnd1Fahrenheit_ShouldReturnNotEqualTemperature() {
+        Temperature temperature21 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Temperature temperature22 = new Temperature(Temperature.Unit.FAHRENHEIT, 1.0);
+        Assertions.assertNotEquals(temperature21, temperature22);
+    }
+
+    @Test
+    void given0FahrenheitAndNull_ShouldReturnNotEqualTemperature() {
+        Temperature temperature1 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Temperature temperature2 = null;
+        Assertions.assertNotEquals(temperature1, temperature2);
+    }
+
+    @Test
+    public void givenType0FahrenheitAnd1Fahrenheit_ShouldReturnEqual() {
+        Temperature temperature1 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Temperature temperature2 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Assertions.assertEquals(temperature1.getClass(), temperature2.getClass());
+    }
+
+    @Test
+    public void givenReference0FahrenheitAnd1Fahrenheit_ShouldReturnNotEqual() {
+        Temperature temperature1 = new Temperature(Temperature.Unit.FAHRENHEIT, 0.0);
+        Temperature temperature2 = new Temperature(Temperature.Unit.FAHRENHEIT, 1.0);
+        Assertions.assertNotEquals(temperature1, temperature2);
+    }
+}
